17c454e2dc1231eb83fd649a87335c4b4f24fea3
XxJaimePCIxX <jaimeolarte888@gmail.com>
2023-04-06 01:25:55 -0500

    se agrega un formulario y agregar items

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import PropTypes from 'prop-types';
 import { useState } from "react";
 
 const courses = [
@@ -19,35 +18,72 @@ const courses = [
         text: 'Curso 4'
     }
 ]
+
 export const CourseList = () => {
+    const [newCourse, setNewCourse] = useState('');
+    const [errorMsg, setErrorMsg] = useState('');
+
+    const handleInputChange = (event) => {
+        setNewCourse(event.target.value);
+    };
+
+    const handleFormSubmit = (event) => {
+        event.preventDefault();
+
+        // Verificar si el curso ya existe en la lista
+        const courseExists = courses.find(course => course.text === newCourse);
+
+        if (courseExists) {
+            setErrorMsg('El curso ya existe en la lista');
+        } else {
+            // Agregar el nuevo curso a la lista de cursos
+            const newCourseItem = {
+                id: courses.length + 1,
+                text: newCourse
+            };
+
+            courses.push(newCourseItem);
+
+            // Actualizar el estado
+            setNewCourse('');
+            setErrorMsg('');
+        }
+    };
+
+    const handleCourseDelete = (id) => {
+        const updatedCourses = courses.filter(course => course.id !== id);
+        setCourses(updatedCourses);
+    };
 
     return (
         <>
             <h1>Lista de cursos</h1>
             <ul>
                 {courses.map((course) => (
-                    <li key={course.id}>{courseItem(course)}</li>
+                    <li key={course.id}>{courseItem(course, handleCourseDelete)}</li>
                 ))}
             </ul>
+            <form onSubmit={handleFormSubmit}>
+                <input type="text" value={newCourse} onChange={handleInputChange} />
+                <button type="submit">Agregar curso</button>
+                {errorMsg && <p>{errorMsg}</p>}
+            </form>
         </>
     )
 }
 
-
-
-function courseItem(course) {
-  const { text, onDeleteClick } = course;
+function courseItem(course, onDeleteClick) {
+  const { id, text } = course;
 
   const handleDeleteClick = () => {
-    onDeleteClick(text.id);
+    onDeleteClick(id);
   }
 
   return (
     <div>
-      <p>Descripción del curso: {course.text}</p>
+      <p>Descripción del curso: {text}</p>
       <button onClick={handleDeleteClick}>Borrar</button>
     </div>
   );
 }
-
 export default courseItem;
